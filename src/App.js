6e48dac7b03e14467b2acfbff0475e354c206623
themightyvicki1 <victoria.greer861@gmail.com>
2024-02-01 15:15:21 -0800

    created header and footer and added bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Dictionary App using React</header>
+      <a href="/" className="btn btn-primary">
+        button
+      </a>
+      <footer>Open sourced coded by Victoria Greer</footer>
+    </div>
+  );
+}
+
+export default App;
