6bdf6e8a063d3cf6ad132c64f950cf992ed2962a
themightyvicki1 <victoria.greer861@gmail.com>
2024-02-01 17:02:01 -0800

    added axios and api url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [word, setWord] = useState("");
 
+  function handleResponse(response) {
+    //[0] b/c stored as an array and we only want first definition (not with this api)
+    console.log(response.data);
+  }
+  //build api axios in search b/c when searching we want to make the axios call
   function search(event) {
     event.preventDefault();
     alert(`searching for ${word}`);
+
+    let apiKey = `0d7079af8c9adb3t72540o1c3a7eb56d`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleWordChange(event) {
